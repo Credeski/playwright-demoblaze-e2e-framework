36dab39e6911a3bb9839437035592e1984e52a18
Solomon Chukwu <theonlycredeski@gmail.com>
2026-07-28 20:47:14 +0100

    Add Checkout tests to verify successful order completion

---
+++ tests/checkout/checkout.spec.ts
@@ -0,0 +1,34 @@
+import { expect } from '@playwright/test';
+import { validCheckout } from '../../src/utils/testData';
+import { CheckoutPage } from '../../src/pages/CheckoutPage';
+import { CartPage } from '../../src/pages/CartPage';
+import { test } from '../../src/fixtures/base.fixture';
+
+
+
+test.describe('Checkout', () => {
+    test('should complete checkout successfully', async ({ page, productInCart }) => {
+        const cartPage = new CartPage(page);
+
+        await page.goto(`${process.env.BASE_URL}/cart.html`);
+        await cartPage.checkout();
+
+        const checkoutPage = new CheckoutPage(page);
+        await checkoutPage.fillOrderForm(validCheckout);
+        await checkoutPage.purchase();
+
+        const confirmationMessage = await checkoutPage.getConfirmationMessage();
+        expect(confirmationMessage).toContain('Thank you for your purchase!');
+
+        const confirmationAmount = await checkoutPage.getConfirmationAmount();
+        expect(confirmationAmount).toContain('Amount: 650 USD');
+
+        const confirmationCardNumber = await checkoutPage.getConfirmationCardNumber();
+        expect(confirmationCardNumber).toContain(validCheckout.creditCard);
+
+        const confirmationName = await checkoutPage.getConfirmationName();
+        expect(confirmationName).toContain(validCheckout.name);
+    });
+});
+
+
